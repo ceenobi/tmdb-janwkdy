5986c04fac84221070e519a102df341434e098a4
ceenobi <cobimbachu@gmail.com>
2023-04-03 16:05:34 +0100

    timeout

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -23,7 +23,7 @@ export default function SearchResult({ searchQuery, setResultBox }) {
         }
       }
       fetchData()
-    }, 2000)
+    }, 1000)
     return () => clearTimeout(delayDebounceFn)
   }, [searchQuery])
 
